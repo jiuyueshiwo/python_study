0168b15bb8914cce0f0511d23902a0f5a09261da
jiuyueshiwo <45483132+jiuyueshiwo@users.noreply.github.com>
2019-05-31 18:24:57 +0800

    破解压缩包密码

--- unzip/ordinary_unzip.py
+++ unzip/ordinary_unzip.py
@@ -2,21 +2,38 @@
 import os
 import subprocess
 import zipfile
-def brutecrack():
-	for a in range(1,10):
-		for b in range(1,10):
-			for c in range(1,10):
-				for d in range(1,10):
-					for e in range(1,10):
-						for f in range(1,10):
+import string
+
+rg = string.digits + string.ascii_letters + string.punctuation
+
+def _brutecrack(rg):
+	for a in rg:
+		for b in rg:
+			for c in rg:
+				for d in rg:
+					for e in rg:
+						for f in rg:
 							passwd=str(a)+str(b)+str(c)+str(d)+str(e)+str(f)
 							command='c:\\Program Files\\7Zip\\7z.exe -p'+passwd+' t C:/Users/Administrator/Desktop/genxi.zip'  #t 表示test，不进行实际解压，只测试密码
-							print(passwd)
+							os.system("cls")
+							print("\r尝试密码为："+passwd, end="")
 							child=subprocess.call(command)
 							#os.popen(command)#这个也可以用,但是不好监控解压状态
-							print(child)
 							if child==0:
+								print(child)
 								print("密码为："+passwd)
-								return
+								return True
+	return False
+
+def brutecrack():
+	rg_temp = string.digits
+	if _brutecrack(rg_temp):
+		return 
+	rg_temp = string.ascii_lowercase
+	if _brutecrack(rg_temp):
+		return
+	rg_temp = rg 
+	_brutecrack(rg_temp)
+	
 if __name__ == '__main__':
 	brutecrack()
